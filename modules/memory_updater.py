00428db9374655b3ecf0ce9806b2d8e1254101c0
Emanuele Rossi <erossi@twitter.com>
2020-10-01 15:11:17 +0000

    Add vanilla RNN memory updater

--- modules/memory_updater.py
+++ modules/memory_updater.py
@@ -7,16 +7,13 @@ class MemoryUpdater(nn.Module):
     pass
 
 
-class GRUMemoryUpdater(MemoryUpdater):
+class SequenceMemoryUpdater(MemoryUpdater):
   def __init__(self, memory, message_dimension, memory_dimension, device):
-    super(GRUMemoryUpdater, self).__init__()
+    super(SequenceMemoryUpdater, self).__init__()
     self.memory = memory
     self.layer_norm = torch.nn.LayerNorm(memory_dimension)
     self.message_dimension = message_dimension
     self.device = device
-
-    self.memory_updater = nn.GRUCell(input_size=message_dimension,
-                                     hidden_size=memory_dimension)
 
   def update_memory(self, unique_node_ids, unique_messages, timestamps):
     if len(unique_node_ids) <= 0:
@@ -46,3 +43,26 @@ class GRUMemoryUpdater(MemoryUpdater):
     updated_last_update[unique_node_ids] = timestamps
 
     return updated_memory, updated_last_update
+
+
+class GRUMemoryUpdater(SequenceMemoryUpdater):
+  def __init__(self, memory, message_dimension, memory_dimension, device):
+    super(GRUMemoryUpdater, self).__init__(memory, message_dimension, memory_dimension, device)
+
+    self.memory_updater = nn.GRUCell(input_size=message_dimension,
+                                     hidden_size=memory_dimension)
+
+
+class RNNMemoryUpdater(SequenceMemoryUpdater):
+  def __init__(self, memory, message_dimension, memory_dimension, device):
+    super(RNNMemoryUpdater, self).__init__(memory, message_dimension, memory_dimension, device)
+
+    self.memory_updater = nn.RNNCell(input_size=message_dimension,
+                                     hidden_size=memory_dimension)
+
+
+def get_memory_updater(module_type, memory, message_dimension, memory_dimension, device):
+  if module_type == "gru":
+    return GRUMemoryUpdater(memory, message_dimension, memory_dimension, device)
+  elif module_type == "rnn":
+    return RNNMemoryUpdater(memory, message_dimension, memory_dimension, device)
